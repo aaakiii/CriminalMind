26ddf6a4e646b4a34d0c0d69b09b5a2b691fa3c9
AKI <a.okada3@gmail.com>
2017-06-08 12:07:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/aki/climinal/CrimeActivity.java
@@ -0,0 +1,23 @@
+package com.example.aki.climinal;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+
+import java.util.UUID;
+
+public class CrimeActivity extends SingleFragmentActivity {
+    private static final String EXTRA_ID = "com.example.aki.climinal.extra_id";
+
+    public static Intent newIntent(Context context, UUID crimeId) {
+        Intent intent = new Intent(context, CrimeActivity.class);
+        intent.putExtra(EXTRA_ID, crimeId);
+        return intent;
+    }
+
+    @Override
+    protected Fragment createFragment(){
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_ID);
+        return CrimeFragment.newInstance(crimeId);
+    }
+}
---
+++ app/src/main/java/com/example/aki/climinal/CrimeFragment.java
@@ -0,0 +1,105 @@
+package com.example.aki.climinal;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static android.app.Activity.RESULT_OK;
+
+/**
+ * Created by AKI on 2017-05-31.
+ */
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+    private static final String ARG_CRIME_ID = "crime_id";
+    private static final String EXTRA_ID = "com.example.aki.climinal.extra_id";
+    public static CrimeFragment newInstance(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID, crimeId);
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setAnswerShowResult();
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+//        UUID id  = (UUID) getActivity().getIntent().getSerializableExtra(CrimeListFragment.EXTRA_ID);
+
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //
+
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //
+                mTitleField.setText(mCrime.getTitle());
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+
+        return v;
+    }
+    public void setAnswerShowResult(){
+        Intent data = new Intent();
+        data.putExtra("key", mCrime.getTitle());
+        data.putExtra("a", mCrime.getDate());
+        data.putExtra("x", mCrime.getId());
+
+//        setResult(RESULT_OK, data);
+    }
+
+}
+
